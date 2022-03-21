7402b866c88bb89da6b445074ea0c8c5fbbe93b6
MousaNageh <200moussa200@gmail.com>
2022-03-21 22:56:29 +0200

    add narbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './views/auth/auth.module';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-  
-    
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
